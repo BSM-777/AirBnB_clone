5e7cf20fced37fafe67db3d473290195fc9cdc90
BSM-777 <marouanebassim12@gmail.com>
2024-03-12 17:13:41 +0000

    New

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,37 @@
 import uuid
 from datetime import datetime
+"""A base_model.py class"""
 
 
 class BaseModel:
-
+    """
+    The base model class that defines all
+    attributes and methods for other classes
+    """
     def __init__(self):
+        """Init"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """
+        prints representation of the cladd
+        """
         return "[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """
+        Updates the public instance attribute `updated_at`
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        returns a dictionary containing all
+        data of __dict__ instance
+        """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
