ade72889c48aab7aa573cbfa42f641170e34b957
BSM-777 <marouanebassim12@gmail.com>
2024-03-12 17:09:09 +0000

    New

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,17 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
+
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
@@ -19,6 +22,7 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
+
 
 if __name__ == "__main__":
     my_model = BaseModel()
@@ -31,4 +35,5 @@ if __name__ == "__main__":
     print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+        print("\t{}: ({}) - {}".format(
+            key, type(my_model_json[key]), my_model_json[key]))
